001133593fe8489b45efd6771ebc11079af78ddb
Zelenskiyas <zelensk1973@gmail.com>
2020-12-21 20:52:48 +0200

    init commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World on pythonenywhere!'
+    return 'Hello World on http://zelenskiy.pythonanywhere.com/!'
 
 
 if __name__ == '__main__':
