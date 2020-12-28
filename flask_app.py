142ffdb936012e9c519e64ed0ee5c9673ab9966c
Zelenskiyas <zelensk1973@gmail.com>
2020-12-28 07:45:30 +0200

    refactor 1

--- flask_app.py
+++ flask_app.py
@@ -4,10 +4,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 # basedir = 'G:\\Projects\\site_src_python\\blog.sqlite'
 basedir = os.path.abspath(__file__)
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.sqlite'
@@ -20,38 +18,43 @@ db = SQLAlchemy(app)
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
+    geo = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
+    # ip_address = db.Column(db.String(40), default='0.0.0.0', nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # ip = db.Column(db.ip)
 
     def __repr__(self):
         return '<Article %r>' % self.id
 
+
 class Visitor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    ip_address = db.Column(db.String(40), default='0.0.0.0')
     title = db.Column(db.String(100), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-
-
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s !</h1>'%name
+    return '<h1>Hello, %s !</h1>' % name
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World on http://zelenskiy.pythonanywhere.com/!'
 
-@app.route('/create',methods=['POST', 'GET'])
+
+@app.route('/create', methods=['POST', 'GET'])
 def create_article():
     if request.method == "POST":
         print(request.user_agent)
-        print(request.remote_addr)
         title = request.form['title']
-        intro = request.form['intro']
+        geo = request.form['geo']
         text = request.form['text']
-        article = Article(title=title, intro=intro, text=text)
+
+        article = Article(title=title, geo=geo, text=text)
         try:
             db.session.add(article)
             db.session.commit()
@@ -62,9 +65,6 @@ def create_article():
 
     else:
         return render_template("create-article.html")
-
-
-
 
 
 if __name__ == '__main__':
